829f36e37187551de1e324178f5db45b84da85d9
NourBesbes <nourbesbes95@gmail.com>
2017-06-14 09:34:21 +0100

    + Company

---
+++ company/controllers/company.js
@@ -0,0 +1,23 @@
+/**
+ * Created by nour on 6/13/17.
+ */
+var express = require('express');
+var company = require('../models/company.js');
+const util = require('util');
+module.exports= {
+
+    addCompany: function (req, res, next) {
+        //TODO:
+    },
+    deleteCompany: function (req, res, next) {
+        //TODO:
+    },
+    FindById: function (req, res, next) {
+        //TODO:
+    },
+    listAll: function(req,res,next) {
+        //TODO: show all companies
+    },
+
+
+}
